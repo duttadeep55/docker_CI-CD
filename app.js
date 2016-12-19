de7470fe238da4d2f1d2d635401d2b795a4f8dc5
duttadeep55 <duttadeep55@outlook.com>
2016-12-19 19:10:55 +0530

    first change test

--- app.js
+++ app.js
@@ -5,12 +5,19 @@
 var express = require('express'),
     app = express();
 
+var os = require("os");
+var hostname = os.hostname();
+
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
     res.render('home', {
   });
+});
+
+app.get('/hostname', function(req, res) {
+    res.send(hostname);
 });
 
 app.listen(3000);
